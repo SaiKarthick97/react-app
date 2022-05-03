348c168adff2042ce9a0883f8d99b3d2819dd7a5
SaiKarthick97 <saikarthick97@gmail.com>
2022-05-03 22:38:48 +0530

    App Component Content Changed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Hello Karthick</h1>
+      <p>Be Happy</p>
     </div>
   );
 }
